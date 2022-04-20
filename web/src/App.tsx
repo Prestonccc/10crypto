bf206f08da7609b27b852821f236e22811fc03b2
Prestonccc <prestonchen1209@gmail.com>
2022-04-20 10:44:31 +0800

    10crypto

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App(){
+    return (
+        <div>
+            <h1>Crypto List</h1>
+            {/* <button className="btn" ref="#">My button</button> */}
+        </div>
+
+    );
+}
+export default App;
